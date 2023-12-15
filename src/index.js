92c47897d40cd0f5fe369682dff7eed6dbd2992b
Jacob Warren <jacobiwarren@gmail.com>
2023-12-15 13:09:08 -0600

    Navbar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import ReactDom from "react-dom"
 import { BrowserRouter as Router } from "react-router-dom"
-import { Main } from "./components"
+import { Main, Navbar } from "./components"
 
 ReactDom.render(
     <Router>
+        <Navbar />
         <Main />
     </Router>,
     document.getElementById("app")
