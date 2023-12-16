3fb268343bc64de8720b41702d37b2466cb6b249
Jacob Warren <jacobiwarren@gmail.com>
2023-12-16 15:19:16 -0600

    changed home page back

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,8 +4,7 @@ const Main = () => {
     
     return (
         <div>
-            <h1>Nicholas David Flurry</h1>
-            <h1>IPv4: 192.19.136.186</h1>
+            <h1>Hi, my name is Jacob.</h1>
         </div>
     )
 }
